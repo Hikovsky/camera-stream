55ee26635aa8e440c29eb70f28545fccf6533767
hikovsky <ospanovkuanyshbek@gmail.com>
2022-12-22 22:40:00 +0600

    copy html file into dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-	mode: 'development',
+	mode: 'production',
 	entry: './src/index.js',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'index.js',
 		publicPath: './',
 	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, 'public', 'index.html'),
+			filename: 'index.html',
+		}),
+	],
 }
